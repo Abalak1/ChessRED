0daf06816fe004e2908ea11ead4f6797fe990d25
FIRST_NAME LAST_NAME <abalaki03@gmail.com>
2022-07-07 19:38:11 +0400

    added bootstrap and styled dropdowns

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("option-one").addEventListener(
+    "click",
+    () => {
+        document.getElementById("xx").textContent = document.getElementById("option-one").textContent
+    },
+    false
+);
+
+document.getElementById("option-two").addEventListener(
+    "click",
+    () => {
+        document.getElementById("xx").textContent = document.getElementById("option-two").textContent
+    },
+    false
+);
+document.getElementById("option-three").addEventListener(
+    "click",
+    () => {
+        document.getElementById("xx").textContent = document.getElementById("option-three").textContent
+    },
+    false
+);
